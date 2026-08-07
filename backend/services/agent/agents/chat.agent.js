6cd704ef79a3926b8ba8c2fa139efd2c98320f85
Abhishekk-AK <abhishek520abc@gmail.com>
2026-08-07 22:29:15 +0530

    add chat agent

--- backend/services/agent/agents/chat.agent.js
+++ backend/services/agent/agents/chat.agent.js
@@ -1,3 +1,21 @@
+import { getModel } from "../config/llmModels.js"
+
 export const chatAgent=async (params) => {
-    
+    const llm=await getModel("chat")
+    const systemPrompt="You are SynthAI, an intelligent AI assistant."
+    const response=await llm.invoke([
+        {
+            "role":"system",
+            "content":systemPrompt
+        },
+        {
+            "role":"human",
+            "content":state.prompt
+        }
+    ])
+
+    return {
+        ...state,
+        aiResponse:response.content
+    }
 }
