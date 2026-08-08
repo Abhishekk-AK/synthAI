1669188c9cfc37aea96b6e439319e302006c06b7
Abhishekk-AK <abhishek520abc@gmail.com>
2026-08-08 23:26:31 +0530

    feat-add sidebar header,new chat button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,9 @@ import api from '../../utils/axios'
 import { FcGoogle } from "react-icons/fc"
 import { useDispatch, useSelector } from 'react-redux'
 import { setUserData } from '../redux/userSlice'
+import SideBar from '../components/SideBar'
+import ChatArea from '../components/ChatArea'
+import Artifact from '../components/Artifact'
 
 
 function Home() {
@@ -33,8 +36,12 @@ function Home() {
     <>
       <div className='h-screen flex bg-[#0d0f14] text-white overflow-hidden'>
 
-      {!userData && 
+      <SideBar />
+      <ChatArea />
+      <Artifact />
       
+      {!userData && 
+
         <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur'>
           <div className='w-[340px] bg-[#13151c] border border-white/[0.08] rounded-2xl p-7 flex flex-col gap-5'>
             <div className='flex flex-col gap-1'>
